951b3ac654581185bea33b59a650d66db4038065
Michael Baumer <meeesterbaumer@gmail.com>
2020-08-18 18:13:01 -0500

    Got department working

--- scripts/Employees/EmployeeList.js
+++ scripts/Employees/EmployeeList.js
@@ -1,21 +1,29 @@
 import { getEmployees, useEmployees } from "./EmployeeData.js"
 import { EmployeeAsHtml } from "./EmployeeHTML.js"
 import { getComputers, useComputers } from "../Computers/ComputerData.js";
+import { getDepartments, useDepartments } from "../Departments/DepartmentData.js";
 
 const contentTarget = document.querySelector(".employeeTarget")
 
 export const renderEmployees = () => {
   getEmployees()
   .then(getComputers)
+  .then(getDepartments)
   .then(() => {
     const empArray = useEmployees()
     const compArray = useComputers()
+    const deptArray = useDepartments()
 
     contentTarget.innerHTML = empArray.map(
       (empObj) => {
         empObj.computer = compArray.find(
           (compObj) => {
             return compObj.id === empObj.computerId
+          }
+        )
+        empObj.department = deptArray.find(
+          (deptObj) => {
+            return deptObj.id === empObj.departmentId
           }
         )
         console.table(empObj)
