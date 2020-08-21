ff6567b7bb07a83cd401a6a4ed9427d1d04edabe
Michael Baumer <meeesterbaumer@gmail.com>
2020-08-21 13:27:09 -0500

    added some quick styling adjustments

--- scripts/Employees/EmployeeHTML.js
+++ scripts/Employees/EmployeeHTML.js
@@ -1,7 +1,7 @@
 export const EmployeeAsHtml = (emp) => {
   return `
   <div class="indEmpRender">
-    <div class="employeeName">${emp.firstName} ${emp.lastName}</div>
+    <h3 class="employeeName">${emp.firstName} ${emp.lastName}</h3>
     <div class="employeeAge">is ${emp.age} years old.</div>
     <div class="employeeComputer">${emp.sex}'s currently using a ${emp.computer.year} ${emp.computer.model}</div>
     <div class="employeeDepartment"> & works in the ${emp.department.name} department</div>
